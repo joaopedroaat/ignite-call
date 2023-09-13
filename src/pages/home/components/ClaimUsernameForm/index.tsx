2bdf36258bb203e986fa4ebecbff5ea98e13f262
João Pedro <jotapedro1231@gmail.com>
2023-09-13 16:31:13 -0300

    Get username and redirect user to register page after ClaimUsernameForm submit.

--- src/pages/home/components/ClaimUsernameForm/index.tsx
+++ src/pages/home/components/ClaimUsernameForm/index.tsx
@@ -4,6 +4,7 @@ import { ArrowRight } from 'phosphor-react'
 import { useForm } from 'react-hook-form'
 import { zodResolver } from '@hookform/resolvers/zod'
 import { z } from 'zod'
+import { useRouter } from 'next/router'
 
 const claimUsernameFormSchema = z.object({
   username: z
@@ -23,13 +24,17 @@ export function ClaimUsernameForm() {
   const {
     register,
     handleSubmit,
-    formState: { errors },
+    formState: { errors, isSubmitting },
   } = useForm<ClaimUsernameFormData>({
     resolver: zodResolver(claimUsernameFormSchema),
   })
 
-  function handlePreRegister(data: ClaimUsernameFormData) {
-    console.log(data)
+  const router = useRouter()
+
+  async function handlePreRegister(data: ClaimUsernameFormData) {
+    const { username } = data
+
+    await router.push('/register?username=' + username)
   }
 
   return (
@@ -42,7 +47,7 @@ export function ClaimUsernameForm() {
           crossOrigin=""
           {...register('username')}
         />
-        <Button size="sm" type="submit">
+        <Button size="sm" type="submit" disabled={isSubmitting}>
           Reservar usuário
           <ArrowRight />
         </Button>
